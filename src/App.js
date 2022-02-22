e1150c2ae8a581fd70d52e59ec58e390d2a148d3
[Alkandari-Y] <[yayalkandari@gmail.com]>
2022-02-22 23:56:21 +0300

    basic demo ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  let color = 'white';
+
+	const divStyle = {
+    backgroundColor: color,
+    height: '100vh',
+    width: '100vw',
+    margin: '50vh 45vw',
+  };
+
+	return (
+    <div style={divStyle}>
+      <div style={{margin: '5px 0'}}>
+        <button>
+                White
+        </button>
+      </div>
+      <div style={{margin: '5px 0'}}>
+        <button>
+                BlueViolet
+        </button>
+      </div>
+      <div  style={{margin: '5px 0'}}>
+        <button>
+                Black
+        </button>
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
